c67e7cd9de61fd9df30bc942697ebb37aa2b8980
Alexandre D. Gomes <alexandredanielgomes524@gmail.com>
2025-04-08 17:47:18 +0100

    Atualizei o projeto

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@ let carrossel = document.querySelectorAll('.imagem')
 let pro = document.getElementById('botao-pro')
 let ant = document.getElementById('botao-ant')
 
+let indicador = document.querySelector('.indicadores')
+let dots = indicador.querySelectorAll('ul li')
+
 let count = carrossel.length
 let ativo = 0
 
@@ -11,6 +14,13 @@ pro.onclick = () => {
 
     ativo = ativo >= count -1 ? 0 : ativo + 1
     carrossel[ativo].classList.add('ativo')
+
+    let dotsVelho = indicador.querySelector('ul li.ativo')
+    dotsVelho.classList.remove('ativo')
+    let dots = indicador.querySelectorAll('ul li')
+    dots[ativo].classList.add('ativo')
+
+    indicador.querySelector('.numero').innerHTML = + (ativo + 1)
 }
 
 ant.onclick = () => {
@@ -19,4 +29,11 @@ ant.onclick = () => {
 
     ativo = ativo <= 0 ? count -1 : ativo - 1
     carrossel[ativo].classList.add('ativo')
+
+    let dotsVelho = indicador.querySelector('ul li.ativo')
+    dotsVelho.classList.remove('ativo')
+    let dots = indicador.querySelectorAll('ul li')
+    dots[ativo].classList.add('ativo')
+
+    indicador.querySelector('.numero').innerHTML = + (ativo + 1)
 }
